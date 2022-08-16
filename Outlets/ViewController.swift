cabdd7e90f0a8e004a15b71137a465c84a0636f0
Alumno <itonygame@gmail.com>
2022-08-16 09:08:14 -0700

    Initial Commit

---
+++ Outlets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Outlets
+//
+//  Created by Alumno on 16/08/22.
+//  Copyright © 2022 César Escoobedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
