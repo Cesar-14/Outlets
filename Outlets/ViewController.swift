dead9567174ff3c383bfa16ec554d585e253f3a7
Cesar Escobedo <cesarescobedo@MacBook-Pro-de-Cesar.local>
2022-08-18 18:05:44 -0700

    primer_commit

--- Outlets/ViewController.swift
+++ Outlets/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+        //Un Outlet es una conexión entre un Conyrol del UI del Storyboard y una variable de código Swift.
+    
+    @IBOutlet weak var lblMiEtiqueta: UILabel!
+    
+    
+    //Esta función se activa ya que cargó en memoria (antes de aparecer en pantalla).
+    //Lo que pongamos aqui sobreescribe lo del Storyboard.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lblMiEtiqueta.text = "Este es texto desde código"
+        lblMiEtiqueta.textColor = UIColor.cyan
     }
 
 
